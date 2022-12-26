16a7895c4c8f4902e5f5ab1294c4e8302465ea79
Breyner Parada <paradabre@gmail.com>
2022-12-26 18:04:52 -0500

    Comments spaces fixed

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -60,7 +60,7 @@ function Comments({comments, isPostingComment, comment, setComment, addComment}:
       {userProfile && (
         <div className='absolute bottom-0 left-0 pb-6 px-2 md:px-10'>
           <form onSubmit={addComment} className='flex gap-4'>
-            <input type="text" value={comment} onChange={e => setComment(e.target.value.trim())} placeholder="Add a comment" className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg' />
+            <input type="text" value={comment} onChange={e => setComment(e.target.value)} placeholder="Add a comment" className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg' />
             <button className='text-md text-gray-400' onClick={addComment}>
               {isPostingComment ? (
                 "Commenting..."
